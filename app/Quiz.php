3c6c98fb0c010432d499eb2abca8422b8af08d60
vinicius <viniciusavila84@gmail.com>
2017-11-21 08:36:11 -0200

    Sprint dia 21.11

--- app/Quiz.php
+++ app/Quiz.php
@@ -11,59 +11,59 @@ class Quiz extends Model {
 
     public function __construct() {
         $this->perguntas = [
-            1 => ['p' => 'De manhã, você', 'r' =>
+            1 => ['p' => 'Como você encara o fato de se apresentar para outras pessoas?', 'r' =>
                 [
-                    'a' => 'Acorda cedo e come frutas cortadas metodicamente.',
-                    'b' => 'Sai da cama com o despertador e se prepara para a batalha da semana.',
-                    'c' => 'Só consegue lembrar do seu nome depois do café.',
-                    'd' => 'Levanta e faz café pra todos da casa.',
-                    'e' => 'Passa o café e conserta um erro no HTML.'
+                    'a' => 'Chato, pois prefiro a vida na matrix.',
+					'b' => 'Impossivel, pois não participo de convencões sociais.',
+                    'c' => 'Nao gosto, pois sou timido, mas isso nao vai me impedir.',
+                    'd' => 'Gosto muito, todos tem o direito de saber quem sou.',
+                    'e' => 'Nao me encaixo em nenhuma resposta.'
                 ]
             ],
-            2 => ['p' => 'Indo para o trabalho você encontra uma senhora idosa caída na rua', 'r' =>
+            2 => ['p' => 'Como você age quando tem que ler um livro?', 'r' =>
                 [
-                    'a' => 'Ela vai atrapalhar seu horário. Oculte o corpo',
-                    'b' => 'Levanta a senhora e jura protegê-la com sua vida.',
-                    'c' => 'Ajuda-a, mas questiona sua real identidade.',
-                    'd' => 'Oferece para caminharem juntos até um destino em comum.',
-                    'e' => 'Testa se ela roda bem no Firefox. Não roda.'
+                    'a' => 'Busco o resumo e leio algumas paginas.',
+                    'b' => 'Se o livro for interessante eu tento ler.',
+                    'c' => 'Leio pois tenho responsabilidades a cumprir.',
+                    'd' => 'Leio para expressar minhas criticas e opinioes.',
+                    'e' => 'Nao me encaixo em nenhuma resposta.'
                 ]
             ],
-            3 => ['p' => 'Chega no prédio e o elevador está cheio', 'r' =>
+            3 => ['p' => '3 - Que tipo de filmes e series você assiste?', 'r' =>
                 [
-                    'a' => 'Convence parte das pessoas a esperarem o próximo.',
-                    'b' => 'Ignora as pessoas no elevador e entra de qualquer forma.',
-                    'c' => 'Você questiona a realidade, as coisas e tudo mais. Sobe de escada.',
-                    'd' => 'Com uma leve intimidação passivo-agressiva, encontra um lugar no elevador.',
-                    'e' => 'Cria um app que mostra a lotação do elevador. Vende o app e fica milionário.'
+                    'a' => 'Documentarios, Ficcao cientifica e batalhas medievais.',
+                    'b' => 'Serial Killers, Terror, Animal planet.',
+                    'c' => 'Comedia, Drama, Suspense.',
+                    'd' => 'Comedia pastelão, Romance, Espirita.',
+                    'e' => 'Nao me encaixo em nenhuma resposta.'
                 ]
             ],
-            4 => ['p' => 'Você chega no trabalho e as convenções sociais te obrigam a puxar assunto', 'r' =>
+            4 => ['p' => 'Como voce age sob pressao?', 'r' =>
                 [
-                    'a' => 'Fala sobre a política, eleições, como tudo é um absurdo.',
-                    'b' => 'Larga uma frase polêmica e vê uma pequena guerra se formar.',
-                    'c' => 'Puxa um assunto e te lembram que já foi discutido semana passada.',
-                    'd' => 'Sugere que os colegas trabalhem na ideia de um novo projeto.',
-                    'e' => 'Desabafa sobre como odeia PHP. Todo mundo na sala adora PHP.'
+                    'a' => 'Frio, calculista, tentando sempre manter a logica.',
+                    'b' => 'Explodo e nao penso em mais nada, fod-se tudo to nem ae.',
+                    'c' => 'Preocupado tentando manter a calma.',
+                    'd' => 'Nervoso tentando reverter a situacao a meu favor.',
+                    'e' => 'Nao me encaixo em nenhuma resposta.'
                 ]
             ],
-            5 => ['p' => 'A pauta pegou o dia todo, mas você está indo para casa', 'r' =>
+            5 => ['p' => 'Que voce gosta de fazer nas horas vagas?', 'r' =>
                 [
-                    'a' => 'Vou chamar aqui o meu Uber.',
-                    'b' => 'Pegarei o bus junto com o resto do povo.',
-                    'c' => 'No ponto de ônibus mais uma vez, espero não errar a linha de novo.',
-                    'd' => 'Vou de carro, mas ofereço uma carona para os colegas.',
-                    'e' => 'Acho que descobri uma forma de fazer aquela senhora rodar no Firefox.'
+                    'a' => 'Jogos, Series, Buscar novidades..',
+                    'b' => 'Ficar sentado em lugares publicos observando as pessoas.',
+                    'c' => 'Ficar com a familia, escutar boa musica, conversar com amigos.',
+                    'd' => 'Viajar, praticar atividades fisicas, ler.',
+                    'e' => 'Nao me encaixo em nenhuma resposta.'
                 ]
             ]
         ];
 
         $this->respostas = [
-            'a' => 'Você é House of Cards: ataca o problema com método e faz de tudo para resolver a situação',
-            'b' => 'Você é Game of Thrones: não tem muita delicadeza nas ações, mas resolve o problema de forma prática',
-            'c' => 'Você é Lost: faz as coisas sem ter total certeza se é o caminho certo ou se faz sentido, mas no final dá tudo certo',
-            'd' => 'Você é Breaking Bad: pra fazer acontecer você toma a liderança, mas sempre contando com seus parceiros',
-            'e' => 'Você é Silicon Valley: vive a tecnologia o tempo todo e faz disso um mantra para cada situação no dia'
+            'a' => 'Indicamos a você buscar algum curso na area de exatas, pois suas respostas praticas podem indicar uma facilidade ao raciocinio logico.',
+            'b' => 'Indicamos a procurar ajuda de um profissional para analise do seu perfil.',
+            'c' => 'Indicamos a busca de cursos voltados aos bastidores de uma empresa, onde seu perfil analitico sera de grande ajuda.',
+            'd' => 'Você geralmente e o centro das atencoes, busque cursos onde você podera expressar suas ideias e opiniões. Cursos onde suas ideias possam ser ouvidas e inspiradoras para inovacões.',
+            'e' => 'Indicamos a procurar ajuda de um profissional para analise do seu perfil.'
         ];
     }
 
